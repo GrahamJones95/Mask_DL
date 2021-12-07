8b2dc2dbe0b855906ba6c445e84f54e6f9420922
Graham Jones <gjjones125@gmail.com>
2021-12-06 21:46:17 -0500

    adapted code for adding masks with 3 classes

--- mask_classifier/Data_Generator/mask.py
+++ mask_classifier/Data_Generator/mask.py
@@ -3,6 +3,7 @@ import sys
 import random
 import argparse
 import numpy as np
+from enum import Enum
 from PIL import Image, ImageFile
 
 __version__ = '0.3.0'
@@ -43,26 +44,35 @@ def cli():
     FaceMasker(pic_path, mask_path, args.show, args.model).mask()
 
 
-def create_mask(image_path):
+def create_mask(image_path,save_path,type):
     pic_path = image_path
-    mask_path = "/media/preeth/Data/prajna_files/mask_creator/face_mask/images/blue-mask.png"
+    #mask_path = "/media/preeth/Data/prajna_files/mask_creator/face_mask/images/blue-mask.png"
+    mask_path = BLUE_IMAGE_PATH
     show = False
     model = "hog"
-    FaceMasker(pic_path, mask_path, show, model).mask()
+    FaceMasker(pic_path, save_path, mask_path, show, model,type).mask()
 
 
 
 class FaceMasker:
-    KEY_FACIAL_FEATURES = ('lips', 'chin')
+    KEY_FACIAL_FEATURES = ('top_lip','nose_bridge','chin')
 
-    def __init__(self, face_path, mask_path, show=False, model='hog'):
+    class Mask_Type(Enum):
+        MASK_TYPE_CORRECT = 1
+        MASK_TYPE_INCORRECT = 2
+        MASK_TYPE_NOMASK = 3
+
+
+    def __init__(self, face_path, save_path, mask_path, show=False, model='hog',type=Mask_Type.MASK_TYPE_CORRECT):
         self.face_path = face_path
+        self.save_path = save_path
         self.mask_path = mask_path
         self.show = show
         self.model = model
         self._face_img: ImageFile = None
         self._mask_img: ImageFile = None
-
+        self.mask_type = type
+        
     def mask(self):
         import face_recognition
 
@@ -70,12 +80,15 @@ class FaceMasker:
         face_locations = face_recognition.face_locations(face_image_np, model=self.model)
         face_landmarks = face_recognition.face_landmarks(face_image_np, face_locations)
         self._face_img = Image.fromarray(face_image_np)
+        #self._face_img.show()
         self._mask_img = Image.open(self.mask_path)
 
         found_face = False
+        #print(face_landmarks.keys())
         for face_landmark in face_landmarks:
             # check whether facial features meet requirement
             skip = False
+            print(face_landmark.keys())
             for facial_feature in self.KEY_FACIAL_FEATURES:
                 if facial_feature not in face_landmark:
                     skip = True
@@ -89,7 +102,8 @@ class FaceMasker:
 
         if found_face:
             if self.show:
-                self._face_img.show()
+                #self._face_img.show()
+                1==1
 
             # save
             self._save()
@@ -97,60 +111,144 @@ class FaceMasker:
             print('Found no face.')
 
     def _mask_face(self, face_landmark: dict):
-        nose_bridge = face_landmark['lips']
-        nose_point = nose_bridge[len(nose_bridge) * 1 // 4]
-        nose_v = np.array(nose_point)
 
-        chin = face_landmark['chin']
-        chin_len = len(chin)
-        chin_bottom_point = chin[chin_len // 2]
-        chin_bottom_v = np.array(chin_bottom_point)
-        chin_left_point = chin[chin_len // 8]
-        chin_right_point = chin[chin_len * 7 // 8]
+        if(self.mask_type  == self.Mask_Type.MASK_TYPE_INCORRECT):
+            """ top_lip = face_landmark['top_lip']
+            nose_bridge = face_landmark['nose_bridge']
 
-        # split mask and resize
-        width = self._mask_img.width
-        height = self._mask_img.height
-        width_ratio = 1.2
-        new_height = int(np.linalg.norm(nose_v - chin_bottom_v))
+            top_lip = top_lip[len(nose_bridge)-1]
+            nose_bridge = nose_bridge[len(nose_bridge)* 1 // 4]
+            nose_point = ((top_lip[0]+nose_bridge[0])//2,(top_lip[1]+nose_bridge[1])//2) """
+            
+            top_lip = face_landmark['top_lip']
+            nose_bridge = face_landmark['nose_bridge']
 
-        # left
-        mask_left_img = self._mask_img.crop((0, 0, width // 2, height))
-        mask_left_width = self.get_distance_from_point_to_line(chin_left_point, nose_point, chin_bottom_point)
-        mask_left_width = int(mask_left_width * width_ratio)
-        mask_left_img = mask_left_img.resize((mask_left_width, new_height))
+            #print(top_lip)
+            #print(nose_bridge)
+            nose_points = [point[1] for point in nose_bridge]
+            min_index = np.argmax(nose_points)
 
-        # right
-        mask_right_img = self._mask_img.crop((width // 2, 0, width, height))
-        mask_right_width = self.get_distance_from_point_to_line(chin_right_point, nose_point, chin_bottom_point)
-        mask_right_width = int(mask_right_width * width_ratio)
-        mask_right_img = mask_right_img.resize((mask_right_width, new_height))
+            lip_point = top_lip[(len(top_lip) - 1) // 2]
+            min_point = nose_bridge[min_index]
 
-        # merge mask
-        size = (mask_left_img.width + mask_r0980iight_img.width, new_height)
-        mask_img = Image.new('RGBA', size)
-        mask_img.paste(mask_left_img, (0, 0), mask_left_img)
-        mask_img.paste(mask_right_img, (mask_left_img.width, 0), mask_right_img)
+            #print(lip_point)
+            #print(min_point)
 
-        # rotate mask
-        angle = np.arctan2(chin_bottom_point[1] - nose_point[1], chin_bottom_point[0] - nose_point[0])
-        rotated_mask_img = mask_img.rotate(angle, expand=True)
+            nose_point = ((lip_point[0]+min_point[0])//2,(lip_point[1]+min_point[1])//2)
 
-        # calculate mask location
-        center_x = (nose_point[0] + chin_bottom_point[0]) // 2
-        center_y = (nose_point[1] + chin_bottom_point[1]) // 2
+            nose_v = np.array(nose_point)
 
-        offset = mask_img.width // 2 - mask_left_img.width
-        radian = angle * np.pi / 180
-        box_x = center_x + int(offset * np.cos(radian)) - rotated_mask_img.width // 2
-        box_y = center_y + int(offset * np.sin(radian)) - rotated_mask_img.height // 2
+            chin = face_landmark['chin']
+            chin_len = len(chin)
+            chin_bottom_point = chin[chin_len // 2]
+            chin_bottom_v = np.array(chin_bottom_point)
+            chin_left_point = chin[chin_len // 8]
+            chin_right_point = chin[chin_len * 7 // 8]
 
-        # add mask
-        self._face_img.paste(mask_img, (box_x, box_y), mask_img)
+            # split mask and resize
+            width = self._mask_img.width
+            height = self._mask_img.height
+            width_ratio = 1.2
+            new_height = int(np.linalg.norm(nose_v - chin_bottom_v))
+
+            # left
+            mask_left_img = self._mask_img.crop((0, 0, width // 2, height))
+            mask_left_width = self.get_distance_from_point_to_line(chin_left_point, nose_point, chin_bottom_point)
+            mask_left_width = int(mask_left_width * width_ratio)
+            mask_left_img = mask_left_img.resize((mask_left_width, new_height))
+
+            # right
+            mask_right_img = self._mask_img.crop((width // 2, 0, width, height))
+            mask_right_width = self.get_distance_from_point_to_line(chin_right_point, nose_point, chin_bottom_point)
+            mask_right_width = int(mask_right_width * width_ratio)
+            mask_right_img = mask_right_img.resize((mask_right_width, new_height))
+
+            # merge mask
+            size = (mask_left_img.width + mask_right_img.width, new_height)
+            mask_img = Image.new('RGBA', size)
+            mask_img.paste(mask_left_img, (0, 0), mask_left_img)
+            mask_img.paste(mask_right_img, (mask_left_img.width, 0), mask_right_img)
+
+            # rotate mask
+            angle = np.arctan2(chin_bottom_point[1] - nose_point[1], chin_bottom_point[0] - nose_point[0])
+            rotated_mask_img = mask_img.rotate(angle, expand=True)
+
+            # calculate mask location
+            center_x = (nose_point[0] + chin_bottom_point[0]) // 2
+            center_y = (nose_point[1] + chin_bottom_point[1]) // 2
+
+            offset = mask_img.width // 2 - mask_left_img.width
+            radian = angle * np.pi / 180
+            box_x = center_x + int(offset * np.cos(radian)) - rotated_mask_img.width // 2
+            box_y = center_y + int(offset * np.sin(radian)) - rotated_mask_img.height // 2
+
+            # add mask
+            self._face_img.paste(mask_img, (box_x, box_y), mask_img)
+        elif(self.mask_type  == self.Mask_Type.MASK_TYPE_CORRECT):
+            nose_bridge = face_landmark['nose_bridge']
+            nose_point = nose_bridge[len(nose_bridge) * 1 // 4]
+
+            nose_v = np.array(nose_point)
+
+            chin = face_landmark['chin']
+            chin_len = len(chin)
+            chin_bottom_point = chin[chin_len // 2]
+            chin_bottom_v = np.array(chin_bottom_point)
+            chin_left_point = chin[chin_len // 8]
+            chin_right_point = chin[chin_len * 7 // 8]
+
+            # split mask and resize
+            width = self._mask_img.width
+            height = self._mask_img.height
+            width_ratio = 1.2
+            new_height = int(np.linalg.norm(nose_v - chin_bottom_v))
+
+            # left
+            mask_left_img = self._mask_img.crop((0, 0, width // 2, height))
+            mask_left_width = self.get_distance_from_point_to_line(chin_left_point, nose_point, chin_bottom_point)
+            mask_left_width = int(mask_left_width * width_ratio)
+            mask_left_img = mask_left_img.resize((mask_left_width, new_height))
+
+            # right
+            mask_right_img = self._mask_img.crop((width // 2, 0, width, height))
+            mask_right_width = self.get_distance_from_point_to_line(chin_right_point, nose_point, chin_bottom_point)
+            mask_right_width = int(mask_right_width * width_ratio)
+            mask_right_img = mask_right_img.resize((mask_right_width, new_height))
+
+            # merge mask
+            size = (mask_left_img.width + mask_right_img.width, new_height)
+            mask_img = Image.new('RGBA', size)
+            mask_img.paste(mask_left_img, (0, 0), mask_left_img)
+            mask_img.paste(mask_right_img, (mask_left_img.width, 0), mask_right_img)
+
+            # rotate mask
+            angle = np.arctan2(chin_bottom_point[1] - nose_point[1], chin_bottom_point[0] - nose_point[0])
+            rotated_mask_img = mask_img.rotate(angle, expand=True)
+
+            # calculate mask location
+            center_x = (nose_point[0] + chin_bottom_point[0]) // 2
+            center_y = (nose_point[1] + chin_bottom_point[1]) // 2
+
+            offset = mask_img.width // 2 - mask_left_img.width
+            radian = angle * np.pi / 180
+            box_x = center_x + int(offset * np.cos(radian)) - rotated_mask_img.width // 2
+            box_y = center_y + int(offset * np.sin(radian)) - rotated_mask_img.height // 2
+
+            # add mask
+            self._face_img.paste(mask_img, (box_x, box_y), mask_img)
+        else:
+            1 == 1
 
     def _save(self):
-        path_splits = os.path.splitext(self.face_path)
-        new_face_path = path_splits[0] + '-with-mask' + path_splits[1]
+        str_path = self.face_path.replace('/','.')
+        split = str_path.split('.')
+        
+        location = self.save_path + "without_mask/"
+        if(self.mask_type == self.Mask_Type.MASK_TYPE_INCORRECT):
+            location = self.save_path + "with_mask_incorrect/"
+        elif(self.mask_type  == self.Mask_Type.MASK_TYPE_CORRECT):
+            location = self.save_path + "with_mask_correct/"
+        new_face_path = location + split[-2] + "-with_mask" + "." + split[-1]
         self._face_img.save(new_face_path)
         print(f'Save to {new_face_path}')
 
---
+++ mask_classifier/Data_Generator/preview_mask.py
@@ -0,0 +1,19 @@
+from matplotlib import pyplot as plt
+import mask
+import os
+import random
+
+folder_path = "my_images/source"
+dest_path = "my_images/"
+#dist_path = "/home/preeth/Downloads"
+
+#c = 0
+images = [os.path.join(folder_path, f) for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
+
+for i in range(len(images)):
+    print("the path of the image is", images[i])
+    type = random.choice(list(mask.FaceMasker.Mask_Type))
+    #type = mask.FaceMasker.Mask_Type.MASK_TYPE_INCORRECT
+    mask.create_mask(images[i],dest_path,type = type)
+    
+
